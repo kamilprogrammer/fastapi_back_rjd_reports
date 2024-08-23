75a656599b75e4aef2db7fde7e03bc44cb6169e4
Kamel Rifai <86717843+kamilprogrammer@users.noreply.github.com>
2024-08-23 17:57:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ import uvicorn
 
 
 app = FastAPI()
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=[""],
+allow_headers=["*"],
+)
+
 
 models.Base.metadata.create_all(bind=engine)
 
